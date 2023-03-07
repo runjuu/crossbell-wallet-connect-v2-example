3d288d15f7b8aebbbdf3c19ce768270ce5c8ab69
Runjuu <hi@runjuu.com>
2023-03-06 17:08:44 -0800

    init project from https://github.com/WalletConnect/web3modal/tree/V2/examples/html

---
+++ main.js
@@ -0,0 +1,29 @@
+import { SafeConnector } from '@gnosis.pm/safe-apps-wagmi'
+import { configureChains, createClient } from '@wagmi/core'
+import { arbitrum, avalanche, mainnet, polygon } from '@wagmi/core/chains'
+import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/html'
+
+// 1. Define constants
+const projectId = '8e6b5ffdcbc9794bf9f4a1952578365b'
+const chains = [mainnet, polygon, avalanche, arbitrum]
+
+// 2. Configure wagmi client
+const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
+const wagmiClient = createClient({
+    autoConnect: true,
+    connectors: [...modalConnectors({ appName: 'web3Modal', chains }), new SafeConnector({ chains })],
+    provider
+})
+
+// 3. Create ethereum and modal clients
+const ethereumClient = new EthereumClient(wagmiClient, chains)
+export const web3Modal = new Web3Modal(
+    {
+        projectId,
+        walletImages: {
+            safe: 'https://pbs.twimg.com/profile_images/1566773491764023297/IvmCdGnM_400x400.jpg'
+        }
+    },
+    ethereumClient
+)
