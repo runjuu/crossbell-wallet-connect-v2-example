a9b8acc73756b1685d4ec538ac2789625e2c92a8
Runjuu <hi@runjuu.com>
2023-03-06 18:20:25 -0800

    feat: update chains to `crossbell` and specify WalletConnect version to 2

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 import { SafeConnector } from '@gnosis.pm/safe-apps-wagmi'
 import { configureChains, createClient } from '@wagmi/core'
-import { arbitrum, avalanche, mainnet, polygon } from '@wagmi/core/chains'
+import { crossbell } from '@wagmi/core/chains'
 import { EthereumClient, modalConnectors, walletConnectProvider } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/html'
 
 // 1. Define constants
 const projectId = '8e6b5ffdcbc9794bf9f4a1952578365b'
-const chains = [mainnet, polygon, avalanche, arbitrum]
+const chains = [crossbell]
 
 // 2. Configure wagmi client
 const { provider } = configureChains(chains, [walletConnectProvider({ projectId })])
 const wagmiClient = createClient({
     autoConnect: true,
-    connectors: [...modalConnectors({ appName: 'web3Modal', chains }), new SafeConnector({ chains })],
+    connectors: [...modalConnectors({ appName: 'web3Modal', chains, projectId, version: "2" }), new SafeConnector({ chains })],
     provider
 })
 
